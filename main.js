d815c17cb339f5d36cb322674012d3d0419006a3
LastKnight <jujuwiwi2008@gmail.com>
2025-07-30 14:53:31 +0800

    Added page menu functionality
    Added an owl icon that is a menu for the different pages

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
-const btn = document.querySelector("#test");
+const btnMenu = document.querySelector("#menu");
+const btnMenuPagesList = document.querySelector("nav ul");
+const btnAbtOwl = document.querySelector("#abtowl");
+const btnOwlSpecies = document.querySelector("#owlspecies");
+const btnOwlHunt = document.querySelector("#owlhunt");
+const btnFunFacts = document.querySelector("#funfacts");
+let allpages = document.querySelectorAll(".page");
 
-function sayHello()
+// Stop putting () for functions inside event listeners unless it's newly initialised in there
+
+btnMenu.addEventListener("click", toggleMenus);
+
+function toggleMenus()
 {
-    document.open();
+    btnMenuPagesList.classList.toggle("menuShow");
+}
 
-    let userName = prompt("What is your name?");
-    userName = userName.trim();
-    document.writeln("Hi " + userName + '!');
-
-    document.close();
-};
-
-btn.addEventListener("click", function()
+function hideall()
 {
-    sayHello();
-})
+    for (let onepage of allpages)
+    {
+        onepage.style.display = "none";
+    }
+}
+
+function showPage(pgNo)
+{
+    hideall()
+    let onepage = document.querySelector("#page" + pgNo);
+    onepage.style.display = "block";
+}
+
+btnAbtOwl.addEventListener("click", function()
+{
+    showPage(1);
+});
+
+btnOwlSpecies.addEventListener("click", function()
+{
+    showPage(2);
+});
+
+btnOwlHunt.addEventListener("click", function()
+{
+    showPage(3);
+});
+
+btnFunFacts.addEventListener("click", function()
+{
+    showPage(4);
+});
+
+hideall();
+showPage(1);
